de1c013c2e3ffd3e36053e5d029cf8db35052452
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-05-08 19:00:23 +0530

    switched on secure option in res.cookie() for now

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.post("/signup", async (req: Request, res: Response) => {
   console.log(email, password);
   res.cookie("test", "12345", {
     // httpOnly: true, -> for local developement keep it true.
-    // secure: true,
+    secure: true,
     // domain: "https://cookie-testing-g10l.onrender.com",
     domain: "https://cookie-testing-frontend-teal.vercel.app/",
     path: "/",
